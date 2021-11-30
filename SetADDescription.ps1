52fdd4398f54852c756a3d8b88fa28584b544aa2
AHH-BEES <94644788+AHH-BEES@users.noreply.github.com>
2021-11-30 07:40:21 -0600

    Update SetADDescription.ps1
    added option for not needed to run w/ different credentials

--- SetADDescription.ps1
+++ SetADDescription.ps1
@@ -1,7 +1,9 @@
 clear-host
 $PC = read-host "What computer do you want to edit?"
 $des = read-host "What do you want the description to say?"
-$account = read-host "Give me your admin account"
+$account = read-host "Enter your admin account, with domain. (If current account is priviledged then put n/a)"
 
-
-Set-ADComputer -Identity $PC -Credential MW\$account -Description $des
+if ($account !eq 'n/a')
+  Set-ADComputer -Identity $PC -Credential $account -Description $des
+else 
+  Set-ADComputer -Identity $PC  -Description $des
